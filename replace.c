7b2c4aae0e40ec1aa27f002bd74c0edc745c90e1
Melanie Plageman <melanieplageman@gmail.com>
2016-08-24 14:51:30 -0400

    Use memcpy instead of strcat and strcpy to create format output string

--- replace.c
+++ replace.c
@@ -18,6 +18,7 @@ Datum replace(PG_FUNCTION_ARGS) {
   const char *cp;
 
   // upon scan, key stores the text within the format specifier
+  // must be a StringInfoData so appendStringInfoCharMacro can be used
   StringInfoData key;
   // upon scan, output stores the text before and after the format specifier
   StringInfoData output;
@@ -25,21 +26,33 @@ Datum replace(PG_FUNCTION_ARGS) {
   int length = 0;
   int state = 0;
 
+  // tkey will store the text version of the StringInfoData key parsed from the format_string_text
+  text *tkey;
+
+  // get access to the function hstore_fetchval
   PGFunction hstore_fetchval = load_external_function(
       "hstore", "hstore_fetchval", true, NULL);
+  // get user input hstore
   Datum hstore = PG_GETARG_DATUM(1);
 
+  // value will store the result of calling hstore_fetchval with the key
   Datum value;  
+  // strval will store the converted cstring version of the value
   char *strval;
 
+  // formatoutput is the StringInfoData which is used to copy in the text and the value retrieved
+  StringInfoData formatoutput;
+
+  // result is used to store the returned result which is formatoutput converted to text
   text *result;
+
+  // dresult is the datum version of result
+  Datum dresult;
 
   start_ptr = VARDATA_ANY(format_string_text);
   end_ptr = start_ptr + VARSIZE_ANY_EXHDR(format_string_text);
   initStringInfo(&output);
   initStringInfo(&key);
-
-  StringInfoData formatoutput;
 
   for (cp = start_ptr; cp < end_ptr; cp++) {
 
@@ -60,7 +73,7 @@ Datum replace(PG_FUNCTION_ARGS) {
     }
   }
 
-  text *tkey = cstring_to_text_with_len(key.data, key.len);
+  tkey = cstring_to_text_with_len(key.data, key.len);
   value = DirectFunctionCall2(
     hstore_fetchval, hstore, (Datum) tkey);
   if (value == (Datum) 0)
@@ -68,18 +81,25 @@ Datum replace(PG_FUNCTION_ARGS) {
   pfree(key.data);
   
   strval = DatumGetCString(value);
+  int lenval = strlen(strval);
 
   initStringInfo(&formatoutput);
 
-  strncpy(formatoutput.data, output.data, length);
-  formatoutput.data[length] = '\0';
-  strcat(formatoutput.data, strval);
-  strcat(formatoutput.data, output.data+length);
+  enlargeStringInfo(&formatoutput, lenval + length);
+
+  memcpy(formatoutput.data, output.data, length);
+  formatoutput.len += length;
+  formatoutput.data[formatoutput.len] = '\0';
+  memcpy(formatoutput.data + formatoutput.len, strval, lenval);
+  formatoutput.data[formatoutput.len] = '\0';
+  memcpy(formatoutput.data + formatoutput.len, output.data + length, output.len - length);
+  formatoutput.data[formatoutput.len] = '\0';
 
   result = cstring_to_text_with_len(formatoutput.data, formatoutput.len);
+  dresult = (Datum) result;
 
   pfree(output.data);
   pfree(formatoutput.data);
 
-  PG_RETURN_TEXT_P(result);
+  PG_RETURN_TEXT_P(dresult);
 }
